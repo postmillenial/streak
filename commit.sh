adbe742589e1c13a88463558e841192943d12b44
postmillenial <postmillenial@gmail.com>
2016-04-13 01:18:23 -0400

    1460524703

--- commit.sh
+++ commit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+lines=$(ps -ef | grep streak.py | wc -l)
+
+if [ $lines -ne 2 ]; then
+    python /var/streak/streak.py 2>&1 | logger &
+fi
 git add _*
 git commit -m $(date +%s)
 git push
